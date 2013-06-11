172452a79559e1982ace7c9f06a3ea9326fed2d0
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-10 20:55:59 -0400

    tape

--- test/browser.js
+++ test/browser.js
@@ -1,4 +1,4 @@
-var test = require('tap').test
+var test = require('tape').test
   , runnelExport = require('..')
   , runnelkey = require.resolve('..')
   ;
--- test/runnel.js
+++ test/runnel.js
@@ -1,4 +1,4 @@
-var test = require('tap').test
+var test = require('tape').test
   , runnel = require('..')
   , unofails
   , dosfails

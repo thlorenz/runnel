898badea2c84e2784e65d397cb8436c6afc1a8bc
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-06 12:41:19 -0400

    completing runnel seed tests

--- test/runnel-seed.js
+++ test/runnel-seed.js
@@ -21,3 +21,31 @@ test('\nseeding three values', function (t) {
     t.end()
   })
 })
+
+test('\nasync reduce long running computations', function (t) {
+  var computations = [ '1 + 2', '2 + 3', '3 + 4'  ]
+
+  var tasks = computations.map(
+    function (op) {
+      return function compute (acc, cb) {
+        // long running computation ;)
+
+        setTimeout(function () {
+          var args = op.split('+');
+          acc[op] = parseInt(args[0], 10) + parseInt(args[1], 10);
+          cb(null, acc)
+        }, 10);
+
+      };
+    });
+
+  runnel(
+    [ runnel.seed({}) ]
+      .concat(tasks)
+      .concat(function done (err, acc) {
+        t.notOk(err, 'no error')
+        t.deepEqual(acc, { '1 + 2': 3, '2 + 3': 5, '3 + 4': 7 })
+        t.end()
+      })
+  )
+})
